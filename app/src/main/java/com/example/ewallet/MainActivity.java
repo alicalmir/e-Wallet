39fbf283157c3c9d123324368d084bf39cf2dc1d
NaidaFatic <faticnaida@gmail.com>
2021-05-01 00:24:52 +0200

    main_actvity.xml login look"

---
+++ app/src/main/java/com/example/ewallet/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ewallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        // TODO: 5/1/2021
+    }
+    public void register(View view){
+        // TODO: 5/1/2021
+    }
+}
